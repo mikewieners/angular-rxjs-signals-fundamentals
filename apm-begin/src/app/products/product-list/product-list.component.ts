9a2b376bffc8b13ced7f5aed90e530bf85e46ef6
Mike Wieners <mike.wieners@veteransunited.com>
2024-10-25 12:09:24 -0500

    WIP

--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 
 import { NgIf, NgFor, NgClass } from '@angular/common';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
+import { Subscription, tap } from 'rxjs';
 
 @Component({
     selector: 'pm-product-list',
@@ -10,16 +12,32 @@ import { ProductDetailComponent } from '../product-detail/product-detail.compone
     standalone: true,
   imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
 })
-export class ProductListComponent {
-  // Just enough here for the template to compile
+export class ProductListComponent implements OnInit, OnDestroy {
   pageTitle = 'Products';
   errorMessage = '';
+  subscription!: Subscription
+
+  private productService = inject(ProductService);
 
   // Products
   products: Product[] = [];
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
+
+  ngOnInit(): void {
+    this.subscription = this.productService.getProducts()
+      .pipe(
+        tap(() => console.log('In component pipeline'))
+      ).subscribe(products => {
+          this.products = products;
+          console.log(this.products);
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
